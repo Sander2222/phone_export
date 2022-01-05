d8ee18cf155e09bbe7f64f149693d90aed09a505
Sander <86840128+Sander2222@users.noreply.github.com>
2022-01-05 03:02:02 +0100

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,7 @@
+--This is for ESX Version 1.2:
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('phone', 'Phone', 1, 0, 1);
+
+--and this is for ESX Version 1.1:
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('phone', 'Phone', 1, 0, 1);
