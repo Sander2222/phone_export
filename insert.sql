83b575f27cd3347cbca7be8bda898fd1bbfa6318
Sander <86840128+Sander2222@users.noreply.github.com>
2022-01-05 03:03:35 +0100

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -5,3 +5,7 @@ INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('p
 --and this is for ESX Version 1.1:
 
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('phone', 'Phone', 1, 0, 1);
+
+--
+--Note: If you already have this item in your database you dont have to add this SQL
+--
